02a06b04db79bfa25145008e1893b8b17120bc5e
weiwei223-rapper <marslin50207@gmail.com>
2026-04-19 14:03:59 +0800

    Quiz

--- frontend/src/component/Layout.tsx
+++ frontend/src/component/Layout.tsx
@@ -29,6 +29,14 @@ const Layout = () => {
                 }`
                 }
             >Profile</NavLink>
+            <NavLink
+                to="./Quiz"
+                className={({ isActive }) =>
+                `block px-4 py-3 rounded-lg hover:bg-gray-800 transition-colors ${
+                    isActive ? 'bg-gray-800 font-semibold' : ''
+                }`
+                }
+            >Quiz</NavLink>
 
         </nav>
             <main className="flex-1 ml-64 p-8 overflow-auto">
---
+++ frontend/src/pages/Quiz.tsx
@@ -0,0 +1,16 @@
+import "./PageIndex.css"
+
+
+
+const Quiz = () =>{
+    return(
+        <div className="main">
+            <div className="Welcome">
+                <h1>
+                    Welcome Back!
+                </h1>
+            </div>
+        </div>
+    )
+}
+export default Quiz
